a4029ef7f32c89416e17df8502d0d00957d9f010
Dan Best <dilshoddilmurodov799@gmail.com>
2026-01-03 17:26:39 +0500

    new repo

--- src/lib/ProtectedRoute.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import LoginPage from '../pages/LoginPage'
-
-function ProtectedRoute({ children}) {
-  return loggedIn ? children : <LoginPage />
-}
-
-export default ProtectedRoute
--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? 'https://mychat-backend-1-8s9u.onrender.com/api' : "/api",
+  baseURL: 'https://mychat-backend-1-8s9u.onrender.com/api',
   withCredentials: true,
 });
